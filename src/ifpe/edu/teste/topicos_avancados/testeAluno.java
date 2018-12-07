da43aff76c67950f662793ccc47bb5e3ce3ddf8e
jefferson1998 <jefferson05061998@gmail.com>
2018-12-07 20:47:38 -0300

    close #5

--- src/ifpe/edu/teste/topicos_avancados/testeAluno.java
+++ src/ifpe/edu/teste/topicos_avancados/testeAluno.java
@@ -1,41 +1,37 @@
 package ifpe.edu.teste.topicos_avancados;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
-import ifpe.edu.topicos_avancados.model.Professor;
+import ifpe.edu.topicos_avancados.model.Aluno;
 
 public class testeAluno {
 	
 	Aluno aluno = new Aluno ("Asaph Souza", "12456");
-	
+
 	@Test
 	public void testeCadastroValido() {
-		assertTrue(aluno.getPD().cadastrarAluno(aluno));
-
+		assertTrue(aluno.getAD().cadastrarAluno(aluno));
 	}
 	
 	@Test
 	public void testeCadastroInvalido() {
 		Aluno aluno1 = new Aluno("Asaph Souza", "12456");
-		assertFalse(aluno.getPD().cadastrarAluno(aluno1));
+		assertFalse(aluno.getAD().cadastrarAluno(aluno1));
 	}
 	
 	@Test
 	public void testeListagemCorreta() {
-		assertTrue(aluno.getPD().listagemAluno().contains("Asaph"));
+		assertTrue(aluno.getAD().listagemAluno().contains("Asaph"));
 	}
 	
 	@Test
 	public void testeRemovaoValida() {
-		assertTrue(aluno.getPD().removerAluno("12456"));
+		assertTrue(aluno.getAD().removerAluno("12456"));
 	}
 	
 	@Test
 	public void testeRemocaoInvalido() {
-		assertFalse(aluno.getPD().removerAluno("123"));
+		assertFalse(aluno.getAD().removerAluno("123"));
 	}
-
 	
 }
--- src/ifpe/edu/topicos_avancados/model/Aluno.java
+++ src/ifpe/edu/topicos_avancados/model/Aluno.java
@@ -1,9 +1,11 @@
 package ifpe.edu.topicos_avancados.model;
 
+import ifpe.edu.topicos_avancados.model.DAO.AlunoDao;
+
 public class Aluno extends Pessoa{
 
 	private String matricula;
-
+	private AlunoDao AD = AlunoDao.getAlunoDao();
 	public Aluno(String nome, String matricula) {
 		super(nome);
 		this.matricula = matricula;
@@ -15,6 +17,15 @@ public class Aluno extends Pessoa{
 
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
+	}
+
+	public AlunoDao getAD() {
+		return AD;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [Nome=" + super.getNome() + ", matricula=" + this.matricula + "]";
 	}
 	
 }
