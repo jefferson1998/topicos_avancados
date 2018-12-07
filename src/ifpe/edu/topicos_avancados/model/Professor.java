47530577f96bdc3766c950822789b21dad20cd8c
Rodrigo235 <r.rodrigomoliveira@gmail.com>
2018-12-07 19:33:21 -0300

    correcao da forma de guardar

--- src/ifpe/edu/topicos_avancados/model/Professor.java
+++ src/ifpe/edu/topicos_avancados/model/Professor.java
@@ -1,11 +1,10 @@
 package ifpe.edu.topicos_avancados.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import ifpe.edu.topicos_avancados.model.DAO.ProfessorDao;
 
 public class Professor extends Pessoa{
 
-	private List<Professor> listaProfessor = new ArrayList<>();
+	private ProfessorDao PD = ProfessorDao.getProfessorDao();
 	private String codigo;
 
 	public Professor(String nome, String codigo) {
@@ -20,41 +19,10 @@ public class Professor extends Pessoa{
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
-	
-	public boolean cadastrarProfessor(Professor professor) {
-		return addProfessor(professor);
-	}
-	
-	private boolean addProfessor(Professor professor) {
-		
-		if(findByCode(professor.codigo) instanceof Professor) {
-			return false;
-		}
-		
-		return listaProfessor.add(professor);
-	}
-	
-	public String listagemProfessor() {
-		return this.listaProfessor.toArray().toString();
-	}
-	
-	public Professor findByCode(String code) {
-		for (int i = 0; i < listaProfessor.size(); i++) {
-			if(listaProfessor.get(i).codigo.equals(code))
-				return listaProfessor.get(i);
-		}
-		return null;
-	}
-	
-	public boolean removerProfessor(String codigo) {
-		for (int i = 0; i < listaProfessor.size(); i++) {
-			if(listaProfessor.get(i).codigo.equals(codigo)) {
-				listaProfessor.remove(i);
-				return true;
-			}
-			
-		}
-		return false;
+
+	@Override
+	public String toString() {
+		return "Professor [Nome=" + super.getNome() + ", codigo=" + this.codigo + "]";
 	}
 	
 }
