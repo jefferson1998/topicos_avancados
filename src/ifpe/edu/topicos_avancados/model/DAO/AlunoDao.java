4c3415c50a86159a045e36887a87156cd56f611b
Rodrigo235 <r.rodrigomoliveira@gmail.com>
2018-12-07 20:15:01 -0300

    close #2

---
+++ src/ifpe/edu/topicos_avancados/model/DAO/AlunoDao.java
@@ -0,0 +1,49 @@
+package ifpe.edu.topicos_avancados.model.DAO;
+
+import java.util.ArrayList;
+import java.util.List;
+import ifpe.edu.topicos_avancados.model.Aluno;
+
+public class AlunoDao {
+	
+private static AlunoDao AD;
+	
+	private List<Aluno> listaAluno;
+	
+	private AlunoDao() {
+		listaAluno = new ArrayList<>();
+	}
+	
+	public static AlunoDao getAlunoDao() {
+		
+		if(AD != null)
+			return AD;
+		AD = new AlunoDao();
+		return AD;
+		
+	}
+	
+	public boolean cadastrarAluno(Aluno aluno) {
+		return addAluno(aluno);
+	}
+	
+	private boolean addAluno(Aluno aluno) {
+		
+		if(findByCode(aluno.getMatricula()) instanceof Aluno) {
+			return false;
+		}
+		
+		return listaAluno.add(aluno);
+	}
+	
+	public Aluno findByCode(String matricula) {
+		for (int i = 0; i < listaAluno.size(); i++) {
+			if(listaAluno.get(i).getMatricula().equals(matricula))
+				return listaAluno.get(i);
+		}
+		return null;
+	}
+	
+	
+
+}
